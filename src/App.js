9ab5d2a958819c2cbe55421c601cb9fc4c8a6846
Casey Bienvenu <40640991+caseybienvenu@users.noreply.github.com>
2020-04-29 10:54:05 -0600

    change on feature branch (#1)
    Co-authored-by: Casey Bienvenu <casey.bienvenu@schoolmint.net>

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there, Casey
+          Hi there, Casey. I'm from feature branch.
         </p>
         <a
           className="App-link"
